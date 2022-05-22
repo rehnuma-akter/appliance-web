206b699a2d60393ecb698b4de9d680f0f8b55e5a
ruhi <raktterruhi@gmail.com>
2022-05-22 20:52:41 +0600

    fifth commit

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Cart.css'
+const Cart = ({cart}) => {
+    // console.log(cart);
+    let total = 0;
+    let name = 0;
+    for(const product of cart){
+        total = total + product.price;
+    }
+    return (
+        <div className='cart'>
+            <h3>order summary</h3>
+            <p>Selected items: {cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Item Name:{cart.name}</p>
+        </div>
+    );
+};
+
+export default Cart;
+
